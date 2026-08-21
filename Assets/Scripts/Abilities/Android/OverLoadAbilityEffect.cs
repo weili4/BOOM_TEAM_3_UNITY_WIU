85409d3c50688a220d6d08cc5ed27a5439572e97
Christof-Leow <christofleow@gmail.com>
2026-08-21 11:37:42 +0800

    Baseline android Overload ability additions

--- Assets/Scripts/Abilities/Android/OverLoadAbilityEffect.cs
+++ Assets/Scripts/Abilities/Android/OverLoadAbilityEffect.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
 
-public class OverLoadAbilityEffect
+[CreateAssetMenu(fileName = "OverloadAbility", menuName = "Scriptable Objects/Effects/OverloadAbility")]
+
+public class OverLoadAbilityEffect : AbilityEffect
 {
-    
+    public PlayerController controller;
+    public Animator animator;
+    public override void Activate(GameObject user, Vector2 mouseWorldPos)
+    {
+        controller = user.GetComponent<PlayerController>();
+
+        controller.moveSpeed = controller.moveSpeed * 2;
+        animator = user.GetComponent<Animator>();
+
+        animator.speed = animator.speed * 2;
+
+    }
+
+    public override void Deactivate(GameObject user)
+    {
+        controller = user.GetComponent<PlayerController>();
+        controller.moveSpeed = controller.moveSpeed / 2;
+
+        animator = user.GetComponent<Animator>();
+
+        animator.speed = animator.speed / 2;
+    }
 }
